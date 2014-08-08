e82a8d076e8ca1ab4cca1cddd06c9d06e1e4e088
Junru Shao <yz_sjr@sjtu.edu.cn>
2014-08-08 17:59:41 +0800

    Update Max Area Empty Convex Hull.cpp

--- Computational Geometry/Max Area Empty Convex Hull.cpp
+++ Computational Geometry/Max Area Empty Convex Hull.cpp
@@ -2,7 +2,8 @@
  * Time Complexity: O(N ^ 3)
  */
 
- inline bool toUpLeft(const point &a, const point &b) {
+
+inline bool toUpRight(const point &a, const point &b) {
 	int c = sign(b.y - a.y);
 	if (c > 0) return true;
 	return c == 0 && sign(b.x - a.x) > 0;
@@ -11,26 +12,26 @@
 inline bool cmpByPolarAngle(const point &a, const point &b) { // counter-clockwise, shorter first if they share the same polar angle
 	int c = sign(det(a, b));
 	if (c != 0) return c > 0;
-    return sign(b.len() - a.len()) > 0;
+	return sign(b.len() - a.len()) > 0;
 }
 
 double maxEmptyConvexHull(point p[], int N) {
-	static double dp[MAX_NODE][MAX_NODE];
-	static point vec[MAX_NODE];
-	static int seq[MAX_NODE]; // empty triangles formed with (0, 0), vec[o], vec[ seq[i] ]
+	static double dp[133][133];
+	static point vec[133];
+	static int seq[133]; // empty triangles formed with (0, 0), vec[o], vec[ seq[i] ]
 
 	double ans = 0.0;
 	Rep(o, 1, N) {
 		int totVec = 0;
-		Rep(i, 1, N) if (toUpLeft(p[o], p[i])) 
-			vec[++totVec] = p[o] - p[i];
+		Rep(i, 1, N) if (toUpRight(p[o], p[i])) 
+			vec[++totVec] = p[i] - p[o];
 		sort(vec + 1, vec + totVec + 1, cmpByPolarAngle);
 		Rep(i, 1, totVec) Rep(j, 1, totVec) dp[i][j] = 0.0;
-
+		
 		Rep(k, 2, totVec) {
 			int i = k - 1;
 			while (i > 0 && sign( det(vec[k], vec[i]) ) == 0) --i;
-
+			
 			int totSeq = 0;
 			for (int j; i > 0; i = j) {
 				seq[++totSeq] = i;
