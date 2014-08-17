5de1cae8799adbb265ca0b2b33ad097bcb76a7c5
Junru Shao <yz_sjr@sjtu.edu.cn>
2014-08-17 22:30:32 +0800

    Add LaTeX

--- Data Structure/Usage Of Rope.cpp
+++ Data Structure/Usage Of Rope.cpp
@@ -1,11 +1,10 @@
-﻿
-#include <ext/rope>
+﻿#include <ext/rope>
 
 using __gnu_cxx::crope;
 using __gnu_cxx::rope;
 
-a = b.substr(from, len);		// [from, from + len)
-a = b.substr(from);				// [from, from]
-a = b.c_str();					// quite slow and might lead to memory leaks
-a.insert(p, str);				// insert str before position p
-a.erase(i, n);					// erase [i, i + n)
+a = b.substr(from, len);        // [from, from + len)
+a = b.substr(from);             // [from, from]
+a = b.c_str();                  // quite slow and might lead to memory leaks
+a.insert(p, str);               // insert str before position p
+a.erase(i, n);                  // erase [i, i + n)
