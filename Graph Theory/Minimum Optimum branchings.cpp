3f09e14fd937960992dd3b6be252d73a0f51acfb
Junru Shao <yz_sjr@sjtu.edu.cn>
2014-08-20 22:52:30 +0800

    Update Minimum Optimum Branchings.cpp

--- Graph Theory/Minimum Optimum branchings.cpp
+++ Graph Theory/Minimum Optimum branchings.cpp
@@ -1,139 +1,217 @@
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <cmath>
-#include <algorithm>
-using namespace std;
 
-const int INF = 99999999;
+namespace EdmondsAlgorithm { // O(ElogE + V^2)
 
-struct point {
-	double x, y;
-} p[200];
+const int N = 1111;
+const int M = 1111111;
 
-int pre[200]; //记录该节点的前驱
-double graph[200][200], ans; //图数组和结果
-bool visit[110], circle[110]; //visit记录该点有没有被访问过，circle记录改点是不是在一个圈里
-int n, m, root; //顶点数+边数+根节点标号
+int n, m, a, b, c, x[N], y[N], z[N], 
+	edgeCnt, firstEdge[N], from[M], length[M], nextEdge[M],
+	inEdge[N], key[M], delta[M], depth[M], child[M][2],
+	parent[N], choosen[N], degree[N], queue[N];
 
-void dfs(int t) { //一个深度优先搜索，搜索出一个最大的联通空间
-	int i;
-	visit[t] = true;
-	for (i = 1; i <= n; ++i) {
-		if (!visit[i] && graph[t][i] != INF)
-			dfs(i);
+void pass (int x) {
+	if (delta[x] != 0) {
+		key[child[x][0]] += delta[x];
+		delta[child[x][0]] += delta[x];
+		key[child[x][1]] += delta[x];
+		delta[child[x][1]] += delta[x];
+		delta[x] = 0;
 	}
 }
 
-bool check() { //这个函数用来检查最小树形图是否存在，即如果存在，那么一遍dfs后，应该可以遍历到所有的节点
-	memset(visit, false, sizeof(visit));
-	dfs(root);
-
-	for (int i = 1; i <= n; ++i)
-		if (!visit[i])
-			return false;
-	return true;
+int merge (int x, int y) {
+	if (x == 0 or y == 0) {
+		return x ^ y;
+	}
+	if (key[x] > key[y]) {
+		swap(x, y);
+	}
+	pass(x);
+	child[x][1] = merge(child[x][1], y);
+	if (depth[child[x][0]] < depth[child[x][1]]) {
+		swap(child[x][0], child[x][1]);
+	}
+	depth[x] = depth[child[x][1]] + 1;
+	return x;
 }
 
-double dist(int i, int j) {
-	return sqrt(
-		(p[i].x - p[j].x) * (p[i].x - p[j].x) + 
-		(p[i].y - p[j].y) * (p[i].y - p[j].y));
+void addEdge (int u, int v, int w) {
+	from[++ edgeCnt] = u;
+	length[edgeCnt] = w;
+	nextEdge[edgeCnt] = firstEdge[v];
+	firstEdge[v] = edgeCnt;
+	key[edgeCnt] = w;
+	delta[edgeCnt] = 0;
+	depth[edgeCnt] = 0;
+	child[edgeCnt][0] = child[edgeCnt][1] = 0;
+	inEdge[v] = merge(inEdge[v], edgeCnt);
 }
 
-int exist_circle() { //判断图中是不是存在有向圈
-	int i;
-	int j;
-	root = 1;
-	pre[root] = root;
-	for (i = 1; i <= n; ++i) {
-		if (!circle[i] && i != root) {
-			pre[i] = i;
-			graph[i][i] = INF;
+void deleteMin (int &r) {
+	pass(r);
+	r = merge(child[r][0], child[r][1]);
+}
 
-			for (j = 1; j <= n; ++j) {
-				if (!circle[j] && graph[j][i] < graph[pre[i]][i])
-					pre[i] = j;
+int findRoot (int u) {
+	if (parent[u] != u) {
+		parent[u] = findRoot(parent[u]);
+	}
+	return parent[u];
+}
+
+void clear () {
+	edgeCnt = 0;
+	depth[0] = -1;
+	memset(inEdge, 0, sizeof(inEdge));
+	memset(firstEdge, 0, sizeof(firstEdge));
+}
+
+int solve (int root) {
+	int result = 0;
+	for (int i = 0; i < n; ++ i) {
+		parent[i] = i;
+	}
+	while (true) {
+		memset(degree, 0, sizeof(degree));
+		for (int i = 0; i < n; ++ i) {
+			if (i == root or parent[i] != i) {
+				continue;
+			}
+			while (findRoot(from[inEdge[i]]) == findRoot(i)) {
+				deleteMin(inEdge[i]);
+			}
+			choosen[i] = inEdge[i];
+			degree[findRoot(from[choosen[i]])] += 1;
+		}
+		int head = 0, tail = 0;
+		for (int i = 0; i < n; ++ i) {
+			if (i != root and parent[i] == i and degree[i] == 0) {
+				queue[tail ++] = i;
 			}
 		}
-	}  //这个for循环负责找出所有非根节点的前驱节点
-	for (i = 1; i <= n; ++i) {
-		if (circle[i]) continue;
-		memset(visit, false, sizeof(visit));
-		int j = i;
-		while (!visit[j]) {
-			visit[j] = true;
-			j = pre[j];
+		while (head < tail) {
+			if (-- degree[findRoot(from[choosen[queue[head]]])] == 0) {
+				queue[tail ++] = findRoot(from[choosen[queue[head]]]);
+			}
+			head += 1;
 		}
-		if (j == root)
-			continue;
-		return j;
-	} //找圈过程，最后返回值是圈中的一个点
-
-	return -1; //如果没有圈，返回-1
+		bool found = false;
+		for (int i = 0; i < n; ++ i) {
+			if (i != root and parent[i] == i and degree[i] > 0) {
+				found = true;
+				int j = i, temp = 0;
+				do{
+					j = findRoot(from[choosen[j]]);
+					parent[j] = i;
+					deleteMin(inEdge[j]);
+					result += key[choosen[j]];
+					key[inEdge[j]] -= key[choosen[j]];
+					delta[inEdge[j]] -= key[choosen[j]];
+					temp = merge(temp, inEdge[j]);
+				} while (j != i);
+				inEdge[i] = temp;
+			}
+		}
+		if (not found) {
+			break;
+		}
+	}
+	for (int i = 0; i < n; ++ i) {
+		if (i != root and parent[i] == i) {
+			result += key[choosen[i]];
+		}
+	}
+	return result;
 }
 
-void update(int t) { //缩圈之后更新数据
-	int i, j;
-	ans += graph[pre[t]][t];
-	for (i = pre[t]; i != t; i = pre[i]) {
-		ans += graph[pre[i]][i];
-		circle[i] = true;
-	} //首先把圈里的边权全部加起来，并且留出t节点，作为外部接口
-
-	for (i = 1; i <= n; ++i)
-		if (!circle[i] && graph[i][t] != INF)
-			graph[i][t] -= graph[pre[t]][t];
-	//上面这个for循环的作用是对t节点做更新操作，为什么要单独做？你可以看看线面这个循环的跳出条件。
-
-	for (j = pre[t]; j != t; j = pre[j])
-		for (int i = 1; i <= n; ++i) {
-			if (circle[i])
-				continue;
-			if (graph[i][j] != INF)
-				graph[i][t] = min(graph[i][t], graph[i][j] - graph[pre[j]][j]);
-			/**/ //////////////////////////////////////////////////////////////////////////
-			graph[t][i] = min(graph[j][i], graph[t][i]);
-		}
-	//这个循环对圈中的其他顶点进行更新
 }
 
-void solve() {
-	int j;
-	memset(circle, false, sizeof(circle));
-	while ((j = exist_circle()) != -1)
-		update(j);
+namespace ChuLiu { // O(V ^ 3)
 
-	for (j = 1; j <= n; ++j)
-		if (j != root && !circle[j])
-			ans += graph[pre[j]][j];
+const int maxn = 1100;
 
-	printf("%.2f\n", ans);
+int n, m, g[maxn][maxn], used[maxn], pass[maxn], eg[maxn], more, queue[maxn];
+
+void combine(int id, int &sum) {
+	int tot = 0, from, i, j, k;
+	for (; id != 0 && !pass[id]; id = eg[id]) {
+		queue[tot++] = id;
+		pass[id] = 1;
+	}
+	for (from = 0; from < tot && queue[from] != id; from++);
+	if (from == tot) return;
+	more = 1;
+	for (i = from; i < tot; i++) {
+		sum += g[eg[queue[i]]][queue[i]];
+		if (i != from) {
+			used[queue[i]] = 1;
+			for (j = 1; j <= n; j++)
+				if (!used[j])
+					if (g[queue[i]][j] < g[id][j])
+						g[id][j] = g[queue[i]][j];
+		}
+	}
+	for (i = 1; i <= n; i++)
+		if (!used[i] && i != id) {
+			for (j = from; j < tot; j++) {
+				k = queue[j];
+				if (g[i][id] > g[i][k] - g[eg[k]][k])
+					g[i][id] = g[i][k] - g[eg[k]][k];
+			}
+		}
+}
+
+int mdst(int root) { // return the total length of MDST 
+	int i, j, k, sum = 0;
+	memset(used, 0, sizeof(used));
+	for (more = 1; more;) {
+		more = 0;
+		memset(eg, 0, sizeof(eg));
+		for (i = 1; i <= n; i++)
+			if (!used[i] && i != root) {
+				for (j = 1, k = 0; j <= n; j++)
+					if (!used[j] && i != j)
+						if (k == 0 || g[j][i] < g[k][i])
+							k = j;
+				eg[i] = k;
+			}
+		memset(pass, 0, sizeof(pass));
+		for (i = 1; i <= n; i++)
+			if (!used[i] && !pass[i] && i != root)
+				combine(i, sum);
+	}
+	for (i = 1; i <= n; i++)
+		if (!used[i] && i != root)
+			sum += g[eg[i]][i];
+	return sum;
 }
 
 int main() {
-	int i;
-	while (scanf("%d%d", &n, &m) != EOF) {
-		for (i = 0; i <= n; ++i)
-			for (int j = 0; j <= n; ++j)
-				graph[i][j] = INF;
-
-		for (i = 1; i <= n; ++i)
-			scanf("%lf%lf", &p[i].x, &p[i].y);
-
-		for (i = 0; i < m; ++i) {
-			int a, b;
-			scanf("%d%d", &a, &b);
-			graph[a][b] = dist(a, b);
+	int i, j, k, test, cases;
+	cases = 0;
+	scanf("%d", &test);
+	while (test) {
+		test--;
+		scanf("%d%d", &n, &m);
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= n; ++j)
+				g[i][j] = 1000001;
+		for (int i = 1; i <= m; ++i) {
+			scanf("%d%d", &j, &k);
+			j++;
+			k++;
+			scanf("%d", &g[j][k]);
 		}
-
-		root = 1;
-		ans = 0;
-		if (!check())
-			printf("poor snoopy\n");
+		cases++;
+		printf("Case #%d: ", cases);
+		k = mdst(1);
+		if (k > 1000000)
+			printf("Possums!\n");  //===no
 		else
-			solve();
+			printf("%d\n", k);
 	}
 
 	return 0;
+}
+
 }
