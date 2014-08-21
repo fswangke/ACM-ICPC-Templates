56e73b92c6418af64206baad250e3b7f8b0f5d19
niconiconi <niconiconi@sjtu.edu.cn>
2014-08-21 15:25:57 +0800

    Update ExGcd.cpp
    也许会爆int（long long也有可能爆，这时用Java）

--- Math/ExGcd.cpp
+++ Math/ExGcd.cpp
@@ -1,7 +1,7 @@
 
 // ax + by = g = gcd(x, y)
-void exgcd(int x, int y, int &a0, int &b0, int &g) {
-	int a1 = 0, b1 = 1, t;
+void exgcd(long long x, long long y, long long &a0, long long &b0, long long &g) { //Warning: possiblly overflow, even if the answer and the inputs are in range
+	long long a1 = 0, b1 = 1, t;
 	a0 = 1; b0 = 0;
 	while (y != 0) {
 		t = a0 - x / y * a1; a0 = a1; a1 = t;
