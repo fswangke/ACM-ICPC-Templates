545d209fb6a6363c48259e2f46253dbb5b77dfa4
niconiconi <niconiconi@sjtu.edu.cn>
2014-08-31 00:28:55 +0800

    Update StableMatching
    Fixed an issue that would cause the initialization error

--- Graph Theory/StableMatching.cpp
+++ Graph Theory/StableMatching.cpp
@@ -1,8 +1,11 @@
 namespace StableMatching {
 	int pairM[MAXN], pairW[MAXN], p[MAXN];
+    	void clear(int n)
+	{
+        	for (int i = 0; i < n; ++i) pairM[i] = pairW[i] = -1;
+		for (int i = 0; i < n; ++i) p[i] = 0;
+    	}
 	void stableMatching(int n, int orderM[MAXN][MAXN], int preferW[MAXN][MAXN]) {
-		for (int i = 0; i < n; ++i) pairM[i] = -1;
-		for (int i = 0; i < n; ++i) pairW[i] = -1;
 		for (int i = 0; i < n; i++) {
 			while (pairM[i] < 0) {
 				int w = orderM[i][p[i]++], m = pairW[w];
