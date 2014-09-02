124e3a8d358c639cc9d85cd3665aebbde9d600be
Junru Shao <yz_sjr@sjtu.edu.cn>
2014-09-02 20:50:30 +0800

    2014.09.02 20:50

--- Math/FFT_Integer.cpp
+++ Math/FFT_Integer.cpp
@@ -1,80 +1,26 @@
-const int MOD = 3 << 18 | 1;
-const int PRIMITIVE_ROOT = 10;
-const int MAXB = 1 << 20;
-
-namespace PrimitiveRoot {
-//	3 * 2 ^ 21 + 1 is a suitable alternate, PrimitiveRoot = 3
-	int powMod(int a, int d, int p) {
-		int res = 1;
-		for (a %= p; d; d >>= 1) {
-			if (d & 1) res = (long long)res * a % p;
-			a = (long long)a * a % p;
-		}
-		return res;
-	}
-	bool isPrime(int n) {
-		for (int i = 2; i * i <= n; ++i)
-			if (n % i == 0) return false;
-		return true;
-	}
-	int getMod(int downLimit) {
+namespace FFT {
+//	`替代方案: $23068673( = 11 * 2 ^ 21 + 1)$`, 原根为 $3$`
+	const int MOD = 786433; // 3 * 2 ^ 18 + 1
+	const int PRIMITIVE_ROOT = 10;
+	const int MAXB = 1 << 20;
+	int getMod(int downLimit) { // `或者现场自己找一个MOD`
 		for (int c = 3; ; ++c) {
 			int t = (c << 21) | 1;
-			if (isPrime(t) && t >= downLimit)
-				return t;
+			if (t >= downLimit && isPrime(t)) return t;
 		}
-		return -1;
 	}
-	bool isPrimitiveRoot(int a, int mod) {
-		int phi = mod - 1;
-		for (int i = 1; i * i <= phi; ++i) {
-			int j = phi / i;
-			if (i * j != phi) continue;
-			if (i < phi && powMod(a, i, mod) == 1)
-				return false;
-			if (j < phi && powMod(a, j, mod) == 1)
-				return false;
-		}
-		return true;
+	int modInv(int a) {
+		return a <= 1 ? a : (long long) (MOD - MOD / a) * modInv(MOD % a) % MOD;
 	}
-	int getPrimitiveRoot(int p) {
-		int g = 2;
-		while ( !isPrimitiveRoot(g, p) ) ++g;
-		return g;
-	}
-}
-
-namespace FFT {
-	int modinv(int a) {
-		return a <= 1 ? a : (long long) (MOD - MOD / a) * modinv(MOD % a) % MOD;
-	}
-	long long powmod(long long a, int b) {
-		a %= MOD;
-		long long r = 1;
-		while (b) {
-			if (b & 1) {
-				r = r * a % MOD;
-			}
-			if (b >>= 1) {
-				a = a * a % MOD;
-			}
-		}
-		return r;
-	}
-
 	void NTT(int P[], int n, int oper) {
 		for (int i = 1, j = 0; i < n - 1; i++) {
 			for (int s = n; j ^= s >>= 1, ~j & s;);
-			if (i < j) {
-				swap(P[i], P[j]);
-			}
+			if (i < j) swap(P[i], P[j]);
 		}
 		for (int d = 0; (1 << d) < n; d++) {
 			int m = 1 << d, m2 = m * 2;
-			long long unit_p0 = powmod(PRIMITIVE_ROOT, (MOD - 1) / m2);
-			if (oper < 0) {
-				unit_p0 = modinv(unit_p0);
-			}
+			long long unit_p0 = powMod(PRIMITIVE_ROOT, (MOD - 1) / m2);
+			if (oper < 0) unit_p0 = modInv(unit_p0);
 			for (int i = 0; i < n; i += m2) {
 				long long unit = 1;
 				for (int j = 0; j < m; j++) {
@@ -87,28 +33,18 @@ namespace FFT {
 			}
 		}
 	}
-
 	vector<int> mul(const vector<int> &a, const vector<int> &b) {
 		vector<int> ret(max(0, (int) a.size() + (int) b.size() - 1), 0);
 		static int A[MAXB], B[MAXB], C[MAXB];
 		int len = 1;
-		while (len < (int)ret.size()) {
-			len *= 2;
-		}
-		for (int i = 0; i < len; i++) {
-			A[i] = i < (int)a.size() ? a[i] : 0;
-			B[i] = i < (int)b.size() ? b[i] : 0;
-		}
+		while (len < (int)ret.size()) len <<= 1;
+		for (int i = 0; i < len; i++) A[i] = i < (int)a.size() ? a[i] : 0;
+		for (int i = 0; i < len; i++) B[i] = i < (int)b.size() ? b[i] : 0;
 		NTT(A, len, 1);
 		NTT(B, len, 1);
-		for (int i = 0; i < len; i++) {
-			C[i] = (long long) A[i] * B[i] % MOD;
-		}
+		for (int i = 0; i < len; i++) C[i] = (long long) A[i] * B[i] % MOD;
 		NTT(C, len, -1);
-		int inv = modinv(len);
-		for (int i = 0; i < (int)ret.size(); i++) {
-			ret[i] = (long long) C[i] * inv % MOD;
-		}
+		for (int i = 0, inv = modInv(len); i < (int)ret.size(); i++) ret[i] = (long long) C[i] * inv % MOD;
 		return ret;
 	}
 }
--- Math/PrimitiveRoot.cpp
+++ Math/PrimitiveRoot.cpp
@@ -1,44 +1,34 @@
-vector<int> findPrimitiveRoot(int N) {
+vector<int> findPrimitiveRoot(int N) { // `只有 2, 4, $p^n$, $2p^n$ 有原根` 
 	if (N <= 4) return vector<int>(1, max(1, N - 1));
-
-	int phi = N;
+	static int factor[100];
+	int phi = N, totF = 0;
 	{ // check no solution && calculate phi
-		int M = N;
+		int M = N, k = 0;
 		if (~M & 1) M >>= 1, phi >>= 1;
 		if (~M & 1) return vector<int>(0);
-		int k = 0;
 		for (int d = 3; d * d <= M; ++d) if (M % d == 0) {
-			for (; M % d == 0; M /= d);
-			++k;
-			if (k > 1) return vector<int>(0);
-			phi -= phi / d;
-		}
-		if (M > 1) {
-			++k;
+			if (++k > 1) return vector<int>(0);
+			for (phi -= phi / d; M % d == 0; M /= d);
+		} if (M > 1) {
+			if (++k > 1) return vector<int>(0);
 			phi -= phi / M;
 		}
-		if (k > 1) return vector<int>(0);
-	}
-
-	vector<int> ans;
+	} { // factorize phi
+		int M = phi;
+		for (int d = 2; d * d <= M; ++d) if (M % d == 0) {
+			for ( ; M % d == 0; M /= d);
+			factor[++totF] = d;
+		} if (M > 1) factor[++totF] = M;
+	} vector<int> ans;
 	for (int g = 2; g <= N; ++g) if (Gcd(g, N) == 1) {
 		bool good = true;
-		for (int i = 1, p = 1; i < phi; ++i) {
-			p = (LL)p * g % N;
-			if (p == 1) {
-				good = false;
-				break;
-			}
-		}
-		if (good) {
-			for (int i = 1, gp = 1; i <= phi; ++i) {
-				gp = (LL)gp * g % N;
-				if (Gcd(i, phi) == 1)
-					ans.push_back(gp);
-			}
-			break;
-		}
-	}
-	sort(ans.begin(), ans.end());
+		for (int i = 1; i <= totF && good; ++i)
+			if (powMod(g, phi / factor[i], N) == 1) good = false;
+		if (!good) continue;
+		for (int i = 1, gp = g; i <= phi; ++i, gp = (LL)gp * g % N)
+			if (Gcd(i, phi) == 1) ans.push_back(gp);
+		break;
+	} sort(ans.begin(), ans.end());
 	return ans;
 }
+
