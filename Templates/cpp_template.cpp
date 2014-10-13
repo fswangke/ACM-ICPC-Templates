8e7750313faea1094f176f20209a52b832b3c8bd
Junru Shao <yz_sjr@sjtu.edu.cn>
2014-10-13 19:23:38 +0800

    2014.10.13 19:23

--- Templates/cpp_template.cpp
+++ Templates/cpp_template.cpp
@@ -9,38 +9,25 @@
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> pii;
-#ifdef MAX_BUFFER
 namespace BufferedReader {
 	char buff[MAX_BUFFER + 5], *ptr = buff, c; bool flag;
 	bool nextChar(char &c) {
 		if ( (c = *ptr++) == 0 ) {
 			int tmp = fread(buff, 1, MAX_BUFFER, stdin);
-			buff[tmp] = 0;
-			if (tmp == 0) return false;
+			buff[tmp] = 0; if (tmp == 0) return false;
 			ptr = buff; c = *ptr++;
 		} return true;
 	}
 	bool nextUnsignedInt(unsigned int &x) {
-		for ( ; ; ) {
-			if ( !nextChar(c) ) return false;
-			if ('0' <= c && c <= '9') break;
-		}
-		for (x = c - '0'; ; x = x * 10 + c - '0') {
-			if ( !nextChar(c) ) break;
-			if (c < '0' || c > '9') break;
-		} return true;
+		for ( ; ; ) { if ( !nextChar(c) ) return false; if ('0' <= c && c <= '9') break; }
+		for (x = c - '0'; nextChar(c); x = x * 10 + c - '0') if (c < '0' || c > '9') break;
+		return true;
 	}
 	bool nextInt(int &x) {
-		for ( ; ; ) {
-			if ( !nextChar(c) ) return false;
-			if (c == '-' || ('0' <= c && c <= '9')) break;
-		}
-		for ((c == '-') ? (x = 0, flag = true) : (x = c - '0' : flag = false); ; ) {
-			if ( !nextChar(c) ) break;
+		for ( ; ; ) { if ( !nextChar(c) ) return false; if (c == '-' || ('0' <= c && c <= '9')) break; }
+		for ((c == '-') ? (x = 0, flag = true) : (x = c - '0' : flag = false); nextChar(c); x = x * 10 + c - '0')
 			if (c < '0' || c > '9') break;
-			x = x * 10 + c - '0';
-		} if (flag) x = -x;
-		return true;
+		if (flag) x = -x; return true;
 	}
 };
 #endif
